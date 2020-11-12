6da3363f33ae92bedd9ba49ea1edc344f0caaf3a
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-11-12 18:36:32 -0500

    Basic home page component setup

---
+++ components/HomeCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <img :src="home.images[0]" style="width: 200px" alt=""><br />
+    {{ home.title }}<br />
+    {{ home.location.address }} {{ home.location.city }}, {{ home.location.state }}<br />
+    {{ home.guests }} guests <br />
+    {{ home.pricePerNight }} / night
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    home: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,4 @@
+export default {
+  // Creates auto-import
+  components: true
+}

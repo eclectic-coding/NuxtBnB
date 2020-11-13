a7f16c947d7c2268c029bec7cd570ee2be233638
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-11-13 13:49:35 -0500

    Finished pages and routing section

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,9 @@ export default {
         content: 'my website description'
       }
     ]
+  },
+  router: {
+    prefetchLinks: false
   }
 }
 
---
+++ pages/home/_id.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div style="display: flex">
+      <img v-for="image in home.images" :key="image" :src="image" alt="" width="200" height="150" />
+    </div>
+    {{ home.title }}<br />
+    <img src="/images/marker.svg" alt="" width="20" height="20">
+    {{ home.location.address }} {{ home.location.state }} {{ home.location.country }}<br />
+    <img src="/images/star.svg" alt="" width="20" height="20">
+    {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath
+
+  </div>
+</template>
+
+<script>
+import homes from '~/data/homes.json';
+
+export default {
+  head() {
+    return {
+      title: this.home.title
+    };
+  },
+  data() {
+    return {
+      home: {}
+    };
+  },
+  created() {
+    const home = homes.find((home) => home.objectID === this.$route.params.id);
+    this.home = home;
+  }
+};
+</script>
+
+<style scoped>
+
+</style>

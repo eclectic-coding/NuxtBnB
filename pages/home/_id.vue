2e063191972d3a1d373c883378d6a9111268f3bc
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-11-30 11:55:38 -0500

    Add error handling

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -4,8 +4,10 @@
       <img v-for="image in home.images" :key="image" :src="image" alt="" width="200" height="150" />
     </div>
     {{ home.title }}<br />
-    <img src="/images/marker.svg" alt="" width="20" height="20">{{ home.location.address }} {{ home.location.state }} {{ home.location.country }}<br />
-    <img src="/images/star.svg" alt="" width="20" height="20">{{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath
+    <img src="/images/marker.svg" alt="" width="20" height="20">{{ home.location.address }} {{ home.location.state }}
+    {{ home.location.country }}<br />
+    <img src="/images/star.svg" alt="" width="20" height="20">{{ home.guests }} guests, {{ home.bedrooms }} rooms,
+    {{ home.beds }} beds, {{ home.bathrooms }} bath
     <div style="height: 800px; width: 800px" ref="map"></div>
 
   </div>
@@ -13,21 +15,22 @@
 
 <script>
 export default {
-  head(){
+  head() {
     return {
-      title: this.home.title,
-    }
+      title: this.home.title
+    };
   },
-  mounted(){
-    this.$maps.showMap(this.$refs.map, this.home._geoloc.lat, this.home._geoloc.lng)
+  mounted() {
+    this.$maps.showMap(this.$refs.map, this.home._geoloc.lat, this.home._geoloc.lng);
   },
-  async asyncData({ params, $dataApi }){
-    const home = await $dataApi.getHome(params.id)
+  async asyncData({ params, $dataApi, error }) {
+    const response = await $dataApi.getHome(params.id);
+    if (!response.ok) return error({ statusCode: response.status, message: response.statusText });
     return {
-      home
-    }
+      home: response.json
+    };
   }
-}
+};
 </script>
 
 <style scoped>
